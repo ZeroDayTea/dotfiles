a219e3fe6d7184a8be08081eeb51af8a6b9f71db
ZeroDayTea <patrick.dobranowski@gmail.com>
2026-05-27 22:59:00 -0700

    gh support

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -77,7 +77,7 @@
             openssl.dev zlib.dev
 
             # utilities
-            git curl wget vim ripgrep findutils fd pandoc shellcheck unzip mermaid-cli
+            git curl wget vim ripgrep findutils fd pandoc shellcheck unzip mermaid-cli gh
           ] ++ lib.optionals isLinux [
             gcc gdb wireshark docker docker-compose
 
